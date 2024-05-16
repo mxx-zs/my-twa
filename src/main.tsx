161db480a84e636d3f3da8896bf42be75b9d50e6
paul <73805331+mxx-zs@users.noreply.github.com>
2024-05-16 13:09:47 +0800

    移除twa-dev

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
-import '@twa-dev/sdk';
 
 // this manifest is used temporarily for development purposes
 // const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
